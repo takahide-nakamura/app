35058e30635ce74c42a2bcb778937954613d5f18
Takahide Nakamura <takahide.politics@gmail.com>
2026-08-09 14:52:22 +0900

    feat:SNSをヘッダーフッターに表示、indexにSNS表示

---
+++ src/data/socials.ts
@@ -0,0 +1,21 @@
+export const socials = {
+  youtube: {
+    name: "YouTube",
+    url: "",
+  },
+
+  x: {
+    name: "X",
+    url: "",
+  },
+
+  instagram: {
+    name: "Instagram",
+    url: "",
+  },
+
+  note: {
+    name: "note",
+    url: "",
+  },
+};
