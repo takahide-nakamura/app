82c39b19adf74f0f310d69734e3bb7b6862a6299
Takahide Nakamura <takahide.politics@gmail.com>
2026-09-22 16:39:09 +0900

    fix: 規約等を法的整理。

--- src/data/opinionForm.ts
+++ src/data/opinionForm.ts
@@ -5,4 +5,4 @@ export const opinionFormUrl =
  * 市民の声・Googleスプレッドシート「公開データ」のCSVエクスポートURL
  */
 export const opinionPublicCsvUrl =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSU7oB98Tjzj_eMaZFfoRX9H8QlDtxPCXtb3OfgNZzYRQ3y79z23U88vALTaHFWGQEm75CGlSbo6Gk1/pub?gid=329717834&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSU7oB98Tjzj_eMaZFfoRX9H8QlDtxPCXtb3OfgNZzYRQ3y79z23U88vALTaHFWGQEm75CGlSbo6Gk1/pub?gid=967732644&single=true&output=csv";
